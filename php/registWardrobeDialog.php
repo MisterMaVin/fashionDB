7f5a9c9ca67128524dd77324b5ae4c3ec5c4b7b6
MisterMaVin <r90nya@naver.com>
2017-10-18 23:59:39 +0900

    setting docs

---
+++ php/registWardrobeDialog.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+     <meta charset="utf-8">
+</head>
+<body>
+  <form name="inputForm" method="post" target="pagehidden" action="../fashion/registWardrobeProcess.php">
+    <table>
+      <tr>
+        <td>test</td>
+      </tr>
+    </table>
+  </form>
+
+  <input type="Submit" value="Submit" />
+
+  <iframe name="pagehidden" style="visibility:hidden;"></iframe>
+
+</body>
+</html>
